cbd03bb1b62b9a3e36d1b20c4bd96329e7c1811d
pumpkincouture <pumpkincouture@gmail.com>
2014-12-09 16:24:46 -0600

    Updated game loop logic, created helper methods

--- src/Game.java
+++ src/Game.java
@@ -11,26 +11,42 @@ public class Game {
         this.setup = new SetUp();
     }
 
-    public void printStatements() {
+    public void playGame() {
+        printIntro();
+        while (boardIsNotFull()) {
+            getPlayerMoves();
+        }
+    }
+
+    public void getPlayerMoves() {
+         setup.getUI().printUserPrompt(setup.getFirstPlayer().getGamePiece());
+         displayBoard();
+          if (invalidMove()) {
+              System.out.println("IT'S NOT VALID");
+              setup.getUI().printError();
+          } else {
+
+          }
+    }
+
+    private void printIntro() {
         setup.getUI().printWelcomeMessage();
         setup.getUI().printGamePieceAssignment(setup.getFirstPlayer().getGamePiece(), setup.getSecondPlayer().getGamePiece());
         setup.getUI().printRules();
         setup.getUI().printStartingPlayer();
     }
 
-    public void playGame() {
-
-        while (boardIsNotFull()) {
-          setup.getUI().printUserPrompt(setup.getFirstPlayer().getGamePiece());
-          setup.getUI().printBoard(setup.getBoard().getBoardCells());
-        }
-    }
-
-    public void switchPlayer() {
-
+    private boolean invalidMove() {
+        return setup.getBoard().isMoveValid(setup.getUI().captureChoice()) == false;
     }
 
     private boolean boardIsNotFull() {
         return !setup.getBoard().isBoardFull();
     }
+
+    private void displayBoard() {
+        setup.getUI().printBoard(setup.getBoard().getBoardCells());
+    }
+
+
 }
