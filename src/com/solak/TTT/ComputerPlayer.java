e94467872cf584d6d84dc848d19e0c081004e4fb
pumpkincouture <pumpkincouture@gmail.com>
2014-12-16 17:33:49 -0600

    Included computer player class

---
+++ src/com/solak/TTT/ComputerPlayer.java
@@ -0,0 +1,28 @@
+package com.solak.TTT;
+
+import com.solak.TTT.Board;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class ComputerPlayer {
+    private String gamePiece;
+    private Random randomgenerator;
+
+    public ComputerPlayer(String gamePiece) {
+        this.gamePiece = gamePiece;
+        randomgenerator = new Random();
+    }
+
+    public String getGamePiece() {
+        return gamePiece;
+    }
+
+    public String chooseOpenSpace(List<String> openCells) {
+        int index = randomgenerator.nextInt(openCells.size());
+        String space = openCells.get(index);
+        return space;
+    }
+}
