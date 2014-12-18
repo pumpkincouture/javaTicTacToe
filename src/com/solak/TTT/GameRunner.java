3992f5fc364f2e70f19f445030bc21c01e63fe6d
pumpkincouture <pumpkincouture@gmail.com>
2014-12-18 15:40:26 -0600

    Renamed main class to GameRunner class

--- src/com/solak/TTT/GameRunner.java
+++ src/com/solak/TTT/GameRunner.java
@@ -1,13 +1,13 @@
 package com.solak.TTT;
 
-public class Main {
+public class GameRunner {
 
     public static void main(String[] args) {
         Player player1 = new Player("X");
         Player player2 = new Player("O");
         Board board = new Board();
         UserInterface ui = new CommandLineInterface();
-        Game gameRunner = new Game(player1, player2, board, (CommandLineInterface) ui);
-        gameRunner.startGame();
+        Game game = new Game(player1, player2, board, (CommandLineInterface) ui);
+        game.startGame();
     }
 }
