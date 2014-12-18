bcf72a29919f4f0d0ea09661f5547dceb908734b
pumpkincouture <pumpkincouture@gmail.com>
2014-12-18 15:18:49 -0600

    Renamed ui method

--- test/com/solak/TTT/GameTest.java
+++ test/com/solak/TTT/GameTest.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
-    Game gameTest;
-    Player player1 = new Player("X");
-    Player player2 = new Player("O");
-    Board board = new Board();
-    MockUserInterface mockui = new MockUserInterface();
+    private Game gameTest;
+    private Player player1 = new Player("X");
+    private Player player2 = new Player("O");
+    private Board board = new Board();
+    private MockUserInterface mockUi = new MockUserInterface();
 
     public void fillBoard(String choice, String gamePiece) {
         gameTest.placeMoveOnBoard(choice, gamePiece);
@@ -19,87 +19,87 @@ public class GameTest {
 
     @Before
     public void setUp() {
-        gameTest = new Game(player1, player2, board, mockui);
+        gameTest = new Game(player1, player2, board, mockUi);
     }
 
     @Test
     public void displayWelcomeMessage() {
         gameTest.printWelcome();
-        assertEquals(true, mockui.isWelcomeMessageCalled());
+        assertEquals(true, mockUi.isWelcomeMessageCalled());
     }
 
     @Test
     public void displayGamePieces() {
         gameTest.printGamePieces();
-        assertEquals(true, mockui.isGamePieceMessageCalled());
+        assertEquals(true, mockUi.isGamePieceMessageCalled());
     }
 
     @Test
     public void displayStartingPlayer() {
         gameTest.printStartingPlayer();
-        assertEquals(true, mockui.isStartingPlayerCalled());
+        assertEquals(true, mockUi.isStartingPlayerCalled());
     }
 
     @Test
     public void getInitialBoardDisplay() {
         gameTest.displayBoard();
-        assertEquals(true, mockui.isDisplayBoardCalled());
+        assertEquals(true, mockUi.isDisplayBoardCalled());
     }
 
     @Test
     public void displayUserPrompt() {
         gameTest.printPlayerPrompt(player1.getGamePiece());
-        assertEquals(true, mockui.isUserPromptCalled());
+        assertEquals(true, mockUi.isUserPromptCalled());
     }
     @Test
     public void testIfMoveInvalid() {
-        mockui.addNextMove("ppppp");
-        gameTest.printChoiceError(mockui.captureChoice());
-        assertEquals(true, mockui.isDisplayInvalidMoveMessageCalled());
+        mockUi.addNextMove("ppppp");
+        gameTest.printChoiceError(mockUi.captureChoice());
+        assertEquals(true, mockUi.isDisplayInvalidMoveMessageCalled());
     }
 
     @Test
     public void testIfMoveValid() {
-        mockui.addNextMove("5");
-        gameTest.printChoiceError(mockui.captureChoice());
-        assertEquals(false, mockui.isDisplayInvalidMoveMessageCalled());
+        mockUi.addNextMove("5");
+        gameTest.printChoiceError(mockUi.captureChoice());
+        assertEquals(false, mockUi.isDisplayInvalidMoveMessageCalled());
     }
 
     @Test
     public void testForWinner() {
-        mockui.addNextMove("1");
-        mockui.addNextMove("2");
-        mockui.addNextMove("3");
+        mockUi.addNextMove("1");
+        mockUi.addNextMove("2");
+        mockUi.addNextMove("3");
 
         for (int i = 0; i <= 2; i++) {
-            fillBoard(mockui.captureChoice(), player2.getGamePiece());
+            fillBoard(mockUi.captureChoice(), player2.getGamePiece());
         }
 
         gameTest.printGameWinner(gameTest.getWinnerName(player1.getGamePiece(), player2.getGamePiece()));
-        assertEquals(true, mockui.isWinnerStringCalled());
+        assertEquals(true, mockUi.isWinnerStringCalled());
     }
 
     @Test
     public void testForCatsGame() {
-        mockui.addNextMove("1");
-        mockui.addNextMove("4");
-        mockui.addNextMove("3");
-        mockui.addNextMove("8");
-        mockui.addNextMove("9");
-        mockui.addNextMove("5");
-        mockui.addNextMove("2");
-        mockui.addNextMove("6");
-        mockui.addNextMove("7");
+        mockUi.addNextMove("1");
+        mockUi.addNextMove("4");
+        mockUi.addNextMove("3");
+        mockUi.addNextMove("8");
+        mockUi.addNextMove("9");
+        mockUi.addNextMove("5");
+        mockUi.addNextMove("2");
+        mockUi.addNextMove("6");
+        mockUi.addNextMove("7");
 
         for (int i = 0; i <= 4; i++) {
-            fillBoard(mockui.captureChoice(), player1.getGamePiece());
+            fillBoard(mockUi.captureChoice(), player1.getGamePiece());
         }
 
         for (int i = 0; i <= 3; i++) {
-            fillBoard(mockui.captureChoice(), player2.getGamePiece());
+            fillBoard(mockUi.captureChoice(), player2.getGamePiece());
         }
 
         gameTest.printGameWinner(gameTest.getWinnerName(player1.getGamePiece(), player2.getGamePiece()));
-        assertEquals(true, mockui.isCatsGameCalled());
+        assertEquals(true, mockUi.isCatsGameCalled());
     }
 }
