b836c86c309c41cf2aa5002521ea729a11b97fcb
pumpkincouture <pumpkincouture@gmail.com>
2014-12-18 14:26:23 -0600

    Added tests around checking for winner and checking for cats game

--- test/com/solak/TTT/GameTest.java
+++ test/com/solak/TTT/GameTest.java
@@ -13,6 +13,10 @@ public class GameTest {
     Board board = new Board();
     MockUserInterface mockui = new MockUserInterface();
 
+    public void fillBoard(String choice, String gamePiece) {
+        gameTest.placeMoveOnBoard(choice, gamePiece);
+    }
+
     @Before
     public void setUp() {
         gameTest = new Game(player1, player2, board, mockui);
@@ -43,10 +47,59 @@ public class GameTest {
     }
 
     @Test
-    public void getFirstPlayerMove() {
-//        gameTest.chooseMove();
+    public void displayUserPrompt() {
+        gameTest.printPlayerPrompt(player1.getGamePiece());
+        assertEquals(true, mockui.isUserPromptCalled());
+    }
+    @Test
+    public void testIfMoveInvalid() {
+        mockui.addNextMove("ppppp");
+        gameTest.printChoiceError(mockui.captureChoice());
+        assertEquals(true, mockui.isDisplayInvalidMoveMessageCalled());
+    }
+
+    @Test
+    public void testIfMoveValid() {
         mockui.addNextMove("5");
-        System.out.println(mockui.captureChoice());
-//        assertEquals(true, mockui.captureChoice());
+        gameTest.printChoiceError(mockui.captureChoice());
+        assertEquals(false, mockui.isDisplayInvalidMoveMessageCalled());
+    }
+
+    @Test
+    public void testForWinner() {
+        mockui.addNextMove("1");
+        mockui.addNextMove("2");
+        mockui.addNextMove("3");
+
+        for (int i = 0; i <= 2; i++) {
+            fillBoard(mockui.captureChoice(), player2.getGamePiece());
+        }
+
+        gameTest.printGameWinner(gameTest.getWinnerName(player1.getGamePiece(), player2.getGamePiece()));
+        assertEquals(true, mockui.isWinnerStringCalled());
+    }
+
+    @Test
+    public void testForCatsGame() {
+        mockui.addNextMove("1");
+        mockui.addNextMove("4");
+        mockui.addNextMove("3");
+        mockui.addNextMove("8");
+        mockui.addNextMove("9");
+        mockui.addNextMove("5");
+        mockui.addNextMove("2");
+        mockui.addNextMove("6");
+        mockui.addNextMove("7");
+
+        for (int i = 0; i <= 4; i++) {
+            fillBoard(mockui.captureChoice(), player1.getGamePiece());
+        }
+
+        for (int i = 0; i <= 3; i++) {
+            fillBoard(mockui.captureChoice(), player2.getGamePiece());
+        }
+
+        gameTest.printGameWinner(gameTest.getWinnerName(player1.getGamePiece(), player2.getGamePiece()));
+        assertEquals(true, mockui.isCatsGameCalled());
     }
 }
