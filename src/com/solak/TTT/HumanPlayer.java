a648fadf1e09b51d345fc0e7ac93b5d91a5477f9
pumpkincouture <pumpkincouture@gmail.com>
2014-12-20 21:53:43 -0600

    Updated test and player class methods

--- src/com/solak/TTT/HumanPlayer.java
+++ src/com/solak/TTT/HumanPlayer.java
@@ -1,10 +1,14 @@
 package com.solak.TTT;
 
 public class HumanPlayer extends Player {
+    private CommandLineInterface ui;
 
-    public HumanPlayer(String gamePiece) {
+    public HumanPlayer(String gamePiece, CommandLineInterface ui) {
         super(gamePiece);
+        this.ui = ui;
     }
 
-
+    public String getMove() {
+       return ui.captureChoice();
+    }
 }
--- test/com/solak/TTT/HumanPlayerTest.java
+++ test/com/solak/TTT/HumanPlayerTest.java
@@ -7,14 +7,22 @@ import static org.junit.Assert.assertEquals;
 
 public class HumanPlayerTest {
     private HumanPlayer humanPlayerTest;
+    private MockUserInterface mockUI= new MockUserInterface();
 
     @Before
     public void setUp() {
-    humanPlayerTest = new HumanPlayer("X");
+
+    humanPlayerTest = new HumanPlayer("X", mockUI);
     }
 
     @Test
     public void getHumanPlayerGamePiece() {
         assertEquals("X", humanPlayerTest.getGamePiece());
+    }
+
+    @Test
+    public void getHumanPlayerMove() {
+        mockUI.addNextMove("5");
+        assertEquals("5", humanPlayerTest.getMove());
     }
 }
