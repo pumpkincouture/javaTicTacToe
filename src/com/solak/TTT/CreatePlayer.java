4bb0f9335133ab2371386304cf415a784cad4fdb
pumpkincouture <pumpkincouture@gmail.com>
2014-12-21 17:24:03 -0600

    Refactored validation and creation methods

--- src/com/solak/TTT/CreatePlayer.java
+++ src/com/solak/TTT/CreatePlayer.java
@@ -2,31 +2,40 @@ package com.solak.TTT;
 
 public class CreatePlayer {
     private CommandLineInterface ui;
+    private Player player;
 
     public CreatePlayer(CommandLineInterface ui) {
         this.ui = ui;
     }
 
-    public String chooseOpponent() {
+    public Object getChosenOpponent() {
+        checkChoice(chooseOpponent());
+        return getAssignedOpponent();
+    }
+
+    public Object getAssignedOpponent() {
+        return player;
+    }
+
+    private String chooseOpponent() {
         ui.promptForOpponent();
         return ui.captureChoice();
     }
 
-    public String validateChoice(String opponentChoice) {
+    private void checkChoice(String opponentChoice) {
         String capitalizedChoice = opponentChoice.toUpperCase();
         String computerChoice = "C";
         String humanChoice = "H";
+        String invalidChoice = "i";
 
         if (capitalizedChoice.equals(computerChoice)) {
-            return computerChoice;
+            player = new ComputerPlayer("O", new Board());
         } else if (capitalizedChoice.equals(humanChoice)) {
-            return humanChoice;
+            player = new HumanPlayer("O", ui);
         } else {
             ui.printError(opponentChoice);
+            checkChoice(chooseOpponent());
         }
-        return computerChoice;
     }
-
-
 }
 
